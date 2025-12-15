283f446f055509c0957222af40f168d68c86f2b9
James Hughes <james@pyrosoftsolutions.co.uk>
2025-12-15 01:37:08 +0000

    feat: Added watchdog service

--- src/Lazarus/Internal/Watchdog/WatchdogService.cs
+++ src/Lazarus/Internal/Watchdog/WatchdogService.cs
@@ -1,6 +1,30 @@
+using System.Collections.Concurrent;
+
 namespace Lazarus.Internal.Watchdog;
 
-internal class WatchdogService
+internal class WatchdogService<TKey> where TKey : notnull
 {
+    private readonly ConcurrentDictionary<TKey, DateTimeOffset> _lastHeartbeats;
+    private readonly TimeProvider _timeProvider;
 
+    public WatchdogService(TimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+        _lastHeartbeats = new();
+    }
+
+    public void RegisterHeartbeat(TKey service)
+    {
+        DateTimeOffset currentTime = _timeProvider.GetUtcNow();
+        _lastHeartbeats.AddOrUpdate(
+            service,
+            currentTime,
+            (_, existing) => currentTime > existing ? currentTime : existing
+        );
+    }
+
+    public DateTimeOffset? GetLastHeartbeat(TKey service) =>
+        _lastHeartbeats.TryGetValue(service, out DateTimeOffset lastHeartbeat)
+            ? lastHeartbeat
+            : null;
 }
