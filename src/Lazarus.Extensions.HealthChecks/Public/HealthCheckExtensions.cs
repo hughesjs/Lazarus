ddfd125f3b8fa96e9ddc74783835566c705c6f71
James Hughes <james@pyrosoftsolutions.co.uk>
2025-12-16 23:08:43 +0000

    feat: Added health checks based on the heartbeat watchdog

--- src/Lazarus.Extensions.HealthChecks/Public/HealthCheckExtensions.cs
+++ src/Lazarus.Extensions.HealthChecks/Public/HealthCheckExtensions.cs
@@ -1,6 +1,17 @@
+using Lazarus.Extensions.HealthChecks.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 namespace Lazarus.Extensions.HealthChecks.Public;
 
-public class HealthCheckExtensions
+public static class HealthCheckExtensions
 {
-    
+    public static IHealthChecksBuilder AddLazarusHealthcheck<TService>(this IHealthChecksBuilder builder, TimeSpan timeout, string? customName = null, HealthStatus failureStatus = HealthStatus.Unhealthy, IEnumerable<string>? tags = null)
+    {
+        string name = customName ?? $"{typeof(TService).Name} (Lazarus)";
+
+        builder.AddTypeActivatedCheck<LazarusServiceHealthCheck<TService>>(name, failureStatus, tags ?? [], timeout);
+
+        return builder;
+    }
 }
