005601e595a637ef369a1fb653145103b7f515c6
James Hughes <james@pyrosoftsolutions.co.uk>
2025-12-21 05:04:22 +0000

    fix: Fixed race condition with locks

--- src/Lazarus/Internal/Watchdog/InMemoryWatchdogService.cs
+++ src/Lazarus/Internal/Watchdog/InMemoryWatchdogService.cs
@@ -16,16 +16,35 @@ internal class InMemoryWatchdogService<TService>: IWatchdogService<TService>
         _recentHeartbeats = [];
     }
 
-    public void RegisterHeartbeat(Heartbeat report) => _recentHeartbeats.Add(report);
-
-    public Heartbeat? GetLastHeartbeat() => _recentHeartbeats.LastOrDefault();
-
-    public List<Exception> GetExceptionsInWindow()
+    public void RegisterHeartbeat(Heartbeat report)
     {
-        DateTimeOffset cutOff = _timeProvider.GetUtcNow() -  _windowPeriod;
-        List<Heartbeat> heartbeatsInWindow = _recentHeartbeats.Where(hb => hb.EndTime > cutOff).ToList();
-        _recentHeartbeats = heartbeatsInWindow;
+        lock (_recentHeartbeats)
+        {
+            _recentHeartbeats.Add(report);
 
-        return heartbeatsInWindow.Where(hb => hb.Exception is not null).Select(hb => hb.Exception!).ToList();
+            // Prune old heartbeats to prevent unbounded memory growth
+            DateTimeOffset cutOff = _timeProvider.GetUtcNow() - _windowPeriod;
+            _recentHeartbeats = _recentHeartbeats.Where(hb => hb.EndTime > cutOff).ToList();
+        }
+    }
+
+    public Heartbeat? GetLastHeartbeat()
+    {
+        lock (_recentHeartbeats)
+        {
+            return _recentHeartbeats.LastOrDefault();
+        }
+    }
+
+    public IReadOnlyList<Exception> GetExceptionsInWindow()
+    {
+        lock (_recentHeartbeats)
+        {
+            DateTimeOffset cutOff = _timeProvider.GetUtcNow() - _windowPeriod;
+            List<Heartbeat> heartbeatsInWindow = _recentHeartbeats.Where(hb => hb.EndTime > cutOff).ToList();
+            _recentHeartbeats = heartbeatsInWindow;
+
+            return heartbeatsInWindow.Where(hb => hb.Exception is not null).Select(hb => hb.Exception!).ToList().AsReadOnly();
+        }
     }
 }
